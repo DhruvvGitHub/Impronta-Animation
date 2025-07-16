106ec3180768ba514b3596cce46821b430e70e77
DhruvvGitHub <dhruvtanwani967@gmail.com>
2025-07-16 23:06:44 +0530

    implemented animation for 3 headings

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,34 +1,57 @@
-import React, { useEffect, useRef } from 'react'
-import LoadingNavbar from './LoadingNavbar'
-import gsap from 'gsap'
+import React, { useEffect, useRef } from "react";
+import LoadingNavbar from "./LoadingNavbar";
+import gsap from "gsap";
 
 const LoadingScreen = () => {
-  const headingRef = useRef(null);
+  const headingRefs = useRef([]);
+  const mainHeadRef = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
-      headingRef.forEach((heading, index) => {
-              gsap.set(heading, {
-        color: "red",
-        y: 300
-      })
-      })
-    })
+      const tl = gsap.timeline({ delay: 1 });
 
-    return () => ctx.revert()
-  },[])
+      headingRefs.current.forEach((heading, index) => {
+        gsap.set(heading, {
+          y: 1000
+        })
+        tl
+        .to(
+          heading, {
+            y: 0,
+            duration: 1.3,
+            delay: -1.1
+          }
+        )
+        .to(heading, {
+          y: -1000,
+          rotate: -15,
+          duration: 1.3
+        })
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <div className='w-full h-screen flex flex-col justify-between bg-[#265B80] px-8 sm:px-12 md:px-20 lg:px-28 py-6 sm:py-8 md:py-10 lg:py-12'>
-        <LoadingNavbar />
-        <div className='headings-div'>
-          <h2 ref={headingRef} className='text-9xl font-semibold text-white'>Living</h2>
-          <h2 ref={headingRef} className='text-9xl font-semibold text-white'>Space</h2>
-          <h2 ref={headingRef} className='text-9xl font-semibold text-white'>Future</h2>
-          <h2 ref={headingRef} className='text-9xl font-semibold text-white'>Impronta</h2>
-        </div>
+    <div className="w-full h-screen flex flex-col overflow-hidden justify-between bg-[#265B80] px-8 sm:px-12 md:px-20 lg:px-28 py-6 sm:py-8 md:py-10 lg:py-12">
+      <LoadingNavbar />
+      <div className="headings-div relative h-60 flex items-center">
+        {["Living", "Space", "Future"].map((text, i) => (
+          <h2
+            key={i}
+            ref={(el) => {
+              headingRefs.current[i] = el;
+            }}
+            className="text-9xl font-semibold text-white absolute"
+          >
+            {text}
+          </h2>
+        ))}
+        <h2 ref={mainHeadRef} className="text-9xl font-semibold text-white absolute">Impronta</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoadingScreen
+export default LoadingScreen;
