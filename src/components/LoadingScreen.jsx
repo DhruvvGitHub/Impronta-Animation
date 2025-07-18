ec3ac4bdf1e2b23295656d1f99cd915a03ca95a0
DhruvvGitHub <dhruvtanwani967@gmail.com>
2025-07-18 21:30:13 +0530

    created home page

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import LoadingNavbar from "./LoadingNavbar";
 import gsap from "gsap";
+import homeMainImage from "../assets/home_main_img.jpg";
 
 const LoadingScreen = () => {
   const headingRefs = useRef([]);
@@ -8,6 +9,10 @@ const LoadingScreen = () => {
   const navDivRef = useRef(null);
   const leftText = useRef(null);
   const rightText = useRef(null);
+  const logoTitle = useRef(null);
+  const homeRightDiv = useRef(null);
+  const loadingDiv = useRef(null);
+  const homeLeftDiv = useRef(null);
 
   useEffect(() => {
     const ctx = gsap.context(() => {
@@ -66,39 +71,90 @@ const LoadingScreen = () => {
         duration: 1,
         delay: 0.7
       },"a")
+      .to(loadingDiv.current, {
+        top: "-100%"
+      })
+      .from(homeLeftDiv.current, {
+        width: "0%",
+        duration: 1
+      })
+      .from(homeRightDiv.current, {
+        width: "100%",
+        duration: 1
+      })
     });
 
     return () => ctx.revert();
   }, []);
 
   return (
-    <div className="w-full h-screen flex flex-col !overflow-hidden justify-between bg-[#265B80] px-8 sm:px-12 md:px-20 lg:px-28 py-6 sm:py-8 md:py-10 lg:py-12">
-      <div
-        ref={navDivRef}
-        className="text-white flex items-center justify-between relative"
-      >
-        <p ref={leftText}>EST 2002</p>
-        <p ref={rightText}>California</p>
-      </div>
+    <div>
+      <div ref={loadingDiv} className="w-full h-screen absolute flex flex-col !overflow-hidden justify-between bg-[#265B80] px-8 sm:px-12 md:px-20 lg:px-28 py-6 sm:py-8 md:py-10 lg:py-12">
+        <div
+          ref={navDivRef}
+          className="text-white flex items-center justify-between relative"
+        >
+          <p ref={leftText}>EST 2002</p>
+          <p ref={rightText}>California</p>
+        </div>
 
-      <div className="headings-div relative flex-1 flex items-end">
-
-        {["Living", "Space", "Future"].map((text, i) => (
+        <div className="headings-div relative flex-1 flex items-end">
+          {["Living", "Space", "Future"].map((text, i) => (
+            <h2
+              key={i}
+              ref={(el) => (headingRefs.current[i] = el)}
+              className="text-9xl font-semibold text-white absolute"
+            >
+              {text}
+            </h2>
+          ))}
           <h2
-            key={i}
-            ref={(el) => (headingRefs.current[i] = el)}
+            ref={mainHeadRef}
             className="text-9xl font-semibold text-white absolute"
           >
-            {text}
+            Impronta
           </h2>
-        ))}
-       <h2
-  ref={mainHeadRef}
-  className="text-9xl font-semibold text-white absolute"
->
-  Impronta
-</h2>
-
+        </div>
+      </div>
+      <div className="w-full h-screen bg-amber-200">
+        <div className="w-full h-full flex ">
+          <div ref={homeLeftDiv} className="home-left w-[40%] flex flex-col justify-between pb-4">
+            <div className="home-nav flex gap-12">
+              <div className="w-fit py-6 px-4 bg-[#2A2A2A] text-white ">
+                <p className="text-xs">MENU</p>
+              </div>
+              <div className="logo-name">
+                <h5 ref={logoTitle} className="text-2xl font-semibold">
+                  Impronta
+                </h5>
+              </div>
+            </div>
+            <div className="home-middle w-fit  flex items-end text-white">
+              <div className="middle-left px-4 pr-20 py-10 flex items-center gap-10 bg-[#2A2A2A]">
+                <p className="text-xs">01.03</p>
+                <h5>Progetti</h5>
+              </div>
+              <div className="middle-right bg-[#2A2A2A] px-8 py-4">
+                <p className="text-xs underline">TUTTI PROGETTI</p>
+              </div>
+            </div>
+            <div className="home-bottom px-28">
+              <h5 className="text-2xl font-semibold">Living</h5>
+              <h5 className="text-2xl font-semibold">Space</h5>
+              <h5 className="text-2xl font-semibold">Future</h5>
+            </div>
+          </div>
+          <div
+            ref={homeRightDiv}
+            className="home-right w-[60%] h-full bg-zinc-500"
+          >
+            <img
+              src={homeMainImage}
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
