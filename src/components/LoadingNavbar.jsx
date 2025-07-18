6d663ba3512f750cb81efd01f649d44ed901b664
DhruvvGitHub <dhruvtanwani967@gmail.com>
2025-07-18 22:29:11 +0530

    removed unncessary code

--- src/components/LoadingNavbar.jsx
+++ src/components/LoadingNavbar.jsx
@@ -1,24 +1,8 @@
-import gsap from "gsap";
-import React, { useEffect, useRef } from "react";
+
 
 const LoadingNavbar = ({navDivRef}) => {
 
 
-// useEffect(() => {
-//   let ctx = gsap.context(() => {
-//     const screenHeight = window.innerHeight;
-
-//     gsap.from([leftText.current,rightText.current ], {
-//       y: screenHeight/2,
-//       stagger: 0.15,
-//       duration: 1,
-//       ease: "power3.inOut",
-//       delay: 0.3
-//     });
-//   });
-
-//   return () => ctx.revert();
-// }, []);
 
 
 
